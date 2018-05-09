7bec4919433a4e9937b572db3182cf68dab879b2
gary091239 <gary091239@gmail.com>
2018-05-09 20:29:34 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  junyi
+//
+//  Created by Gary on 2018/5/9.
+//  Copyright © 2018年 Gary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
